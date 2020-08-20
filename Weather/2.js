a5b154143f1cd4d314282d2832599596f69b7605
EshanBahna <65206335+EshanBahna@users.noreply.github.com>
2020-08-21 00:44:27 +0530

    Add files via upload

--- Weather/2.js
+++ Weather/2.js
@@ -3,7 +3,7 @@ let weather = function () {
     //creating the object
     let xhr = new XMLHttpRequest();
 
-    let city = document.querySelector('#txt').value;
+    let city = document.querySelector('#txt').value || "Delhi";
 
     let url = "http://api.openweathermap.org/data/2.5/weather?appid=4c24bd22a911ec1121356dbbbf90b704&units=metric&q=" + city;
 
@@ -31,27 +31,47 @@ let jsondomlogic = (jsonref) => {
 
     let parent = document.querySelector('#info-block');
     parent.style.visibility = "Visible";
+    
     //City Name
+
+    //day chart
+    let days = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+    var today = new Date();
+    var day = today.getDay();
+    var time = today.getHours() + ":" + today.getMinutes();
+
     let name = document.querySelector('#city-name');
-     name.innerHTML = jsonref.name;
+     name.children[0].innerHTML = jsonref.name;
+     name.children[1].innerHTML = days[day] + " " + time;
    
 
     //Temprature
     let temp = jsonref.main.temp;
-    parent.children[0].children[1].innerHTML = temp;
+    parent.children[0].children[1].innerHTML = temp + "&#8451";
+
+    //Feels Like
+    let temp_like = jsonref.main.feels_like;
+    parent.children[1].children[1].innerHTML = temp + "&#8451";
     
 
     //Humidity
     let humidity = jsonref.main.humidity;
-    parent.children[1].children[1].innerHTML = humidity;
+    parent.children[2].children[1].innerHTML = humidity + "%";
 
 
-    //Preassure
-    let pressure = jsonref.main.pressure;
-    parent.children[2].children[1].innerHTML = pressure;    
-      
-    document.querySelector('#txt').value = "";
-
+    //Rain
+    let rain = jsonref.weather[0].description;
+    parent.children[3].children[1].innerHTML = rain;  
     
+    //wind speed
+    let wind = jsonref.wind.speed;
+    parent.children[4].children[1].innerHTML = wind + " km/h"; 
+    
+    //Visibility
+    let visible = jsonref.visibility;
+    parent.children[5].children[1].innerHTML = visible/1000 + " km/h"; 
+
+    document.querySelector('#txt').value = "";
 
 }
